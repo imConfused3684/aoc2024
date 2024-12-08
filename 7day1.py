397084145aa73d640dfda0b0c2a8a8992b073ee9
imConfused3684 <oleggrirorev@yandex.ru>
2024-12-09 01:10:22 +0400

    day 7 fix

--- 7day1.py
+++ 7day1.py
@@ -1,78 +1,35 @@
 from getinput import getinput
 import operator
+from typing import Iterable
 
-arr = [1, 2, 3, 4]
-# * * *
-# * * +
-# * + *
-# * + +
-# + * *
-# + * +
-# + + *
-# + + +
+opers = { 0 : operator.add, 1 : operator.mul }
 
-# 3 = 2**3
+def perebor(base: int, length: int) -> Iterable[list[int]]:
+    for i in range(base**length):
+        t = i
+        symbols = []
+        for j in range(length):
+            t = int(t)
+            symbols.append(int(t % base))
+            t /= base
+        yield symbols
 
-def perebor(variant: list[str]) -> list[str]:
-    one_in_mind = False
-    if variant[-1] == '1': one_in_mind = True; variant[-1] = '0'
-    else: variant[-1] = '1'; return variant
-    
-    for i in reversed(range(len(variant) - 1)):
-        if one_in_mind:
-            if variant[i] == '1': variant[i] = '0'
-            elif variant[i] == '0': one_in_mind = False; variant[i] = '1'
-        else:
-            return variant
+def checkLine(line: list[str]) -> int:
+    line[0] = line[0][:len(line[0]) - 1]
+    result = int(line[0])
+    line = line[1:]
 
-def createArray(length: int, symbol: str) -> list[str]:
-    array = []
-    for i in range(length):
-        array.append(symbol)
-    return array
-
-opers = { '0' : operator.add, '1' : operator.mul }
-def executeOper(op1, oper: str, op2):
-    op1, op2 = int(op1), int(op2)
-    return opers[oper](op1, op2)
-
-def executeOpersArray(opersArray: list[str], numsArray) -> int:
-    result = int(numsArray[0])
-    for i in range(1, len(numsArray)):
-        result = executeOper(result, opersArray[i-1], numsArray[i])
-    return result
-        
-
-def checkStr(array: list[str]) -> int:
-    array[0] = array[0][:len(array[0]) - 1]
-    result = int(array[0])
-    array = array[1:]
-    opersArr = createArray(len(array) - 1, '0')
-    for i in range(2**(len(array) - 1)):
-        if executeOpersArray(opersArr, array) == result: return result
-        perebor( opersArr )
+    for variation in perebor(2, len(line) - 1):
+        i = 1
+        lineRes = int(line[0])
+        for symbol in variation:
+            lineRes = opers[symbol](lineRes, int(line[i]))
+            i += 1
+        if result == lineRes : return result
     return 0
 
 sum = 0
 for str in getinput(7).split('\n'):
     if str == '': continue
-    sum += checkStr(str.split())
+    sum += checkLine(str.split())
 print(sum)
-
-
-# str = '134542353423:'
-# str = str[:len(str)-1]
-# print(str)
-
-# str = 'penis'
-# print(str[1:])
-
-# print(executeOpersArray(['0','0','1'], [8,9,2,3]))
-
-
-# length = 4
-# lengthOper = length - 1
-# opersArr = createArray(lengthOper, '0')
-# for i in range(2**lengthOper):
-#     print( opersArr )
-#     perebor(opersArr)
--- 7day2.py
+++ 7day2.py
@@ -1,84 +1,38 @@
 from getinput import getinput
-import operator
+from typing import Iterable
 
-arr = [1, 2, 3, 4]
-# * * *
-# * * +
-# * + *
-# * + +
-# + * *
-# + * +
-# + + *
-# + + +
+def perebor(base: int, length: int) -> Iterable[list[int]]:
+    for i in range(base**length):
+        t = i
+        symbols = []
+        for j in range(length):
+            t = int(t)
+            symbols.append(int(t % base))
+            t /= base
+        yield symbols
 
-# 3 = 2**3
+def checkLine(line: list[str]) -> int:
+    line[0] = line[0][:len(line[0]) - 1]
+    result = int(line[0])
+    line = line[1:]
 
-def perebor(variant: list[str]) -> list[str]:
-    one_in_mind = False
-    if variant[-1] == '1': one_in_mind = True; variant[-1] = '0'
-    else: variant[-1] = '1'; return variant
-    
-    for i in reversed(range(len(variant) - 1)):
-        if one_in_mind:
-            if variant[i] == '1': variant[i] = '0'
-            elif variant[i] == '0': one_in_mind = False; variant[i] = '1'
-        else:
-            return variant
-
-def createArray(length: int, symbol: str) -> list[str]:
-    array = []
-    for i in range(length):
-        array.append(symbol)
-    return array
-
-opers = { '0' : operator.add, '1' : operator.mul }
-def executeOper(op1, oper: str, op2):
-    op1, op2 = int(op1), int(op2)
-    return opers[oper](op1, op2)
-
-def executeOpersArray(opersArray: list[str], numsArray) -> int:
-    result = int(numsArray[0])
-    for i in range(1, len(numsArray)):
-        result = executeOper(result, opersArray[i-1], numsArray[i])
-    return result
-
-def executeOpersArrayWithConcat(opersArray: list[str], numsArray, concatArray: list[str]) -> int:
-    result = int(numsArray[0])
-    for i in range(1, len(numsArray)):
-        if concatArray[i-1] == '1' :
-            string = str(result) + str(numsArray[i])
-            result = int(string)
-        else:
-            result = executeOper(result, opersArray[i-1], numsArray[i])
-    return result
-        
-
-def checkStr(array: list[str]) -> int:
-    array[0] = array[0][:len(array[0]) - 1]
-    result = int(array[0])
-    array = array[1:]
-    opersArr = createArray(len(array) - 1, '0')
-    for i in range(2**(len(array) - 1)):
-        if executeOpersArray(opersArr, array) == result: return result
-        concatArray = createArray(len(array) - 1, '0')
-        for i in range(2**(len(array) - 1)):
-            if executeOpersArrayWithConcat(opersArr, array, concatArray) == result: return result
-            perebor( concatArray )
-        perebor( opersArr )
+    for variation in perebor(3, len(line) - 1):
+        i = 1
+        lineRes = int(line[0])
+        for symbol in variation:
+            match symbol:
+                case 0:
+                    lineRes += int(line[i])
+                case 1:
+                    lineRes *= int(line[i])
+                case 2:
+                    lineRes = int(str(lineRes) + str(line[i]))
+            i += 1
+        if result == lineRes : return result
     return 0
 
 sum = 0
-index = 0
-for inputstr in getinput(7).split('\n'):
-    print(index)
-    if inputstr == '': continue
-    sum += checkStr(inputstr.split())
-    index += 1
+for line in getinput(7).split('\n'):
+    if line == '': continue
+    sum += checkLine(line.split())
 print(sum)
-# 11387
-
-# a = 2
-# b = 5
-# string = str(a) + str(b)
-# result = int(string)
-# print(result + 1)
